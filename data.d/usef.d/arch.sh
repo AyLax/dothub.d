468c0a35e4ec26526611b9a21ca87c3409fef8e8
AyLax <26481009+AyLax@users.noreply.github.com>
2021-05-21 02:10:04 +0800

    Update arch.sh

--- data.d/usef.d/arch.sh
+++ data.d/usef.d/arch.sh
@@ -58,6 +58,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 # 8.step: switch inner system
 arch-chroot /mnt
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 export LANG=en_US.UTF-8
