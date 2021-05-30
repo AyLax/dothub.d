f11f5fdbb932952d1b10c1d4f343c3c5f76c1634
aylax <zhoubye@foxmail.com>
2021-05-30 05:39:46 +0000

    urxvt > sakura

--- data.d/Res/bash.res
+++ data.d/Res/bash.res
@@ -13,3 +13,4 @@ alias ll='ls -l'
 alias la='ls -la'
 alias grep='grep --color=auto'
 alias vim='nvim'
+
--- data.d/Res/install.sh
+++ data.d/Res/install.sh
@@ -1,8 +1,13 @@
+path="$HOME/dothub.d/data.d/Res"
+cat $path/bash.res > $HOME/.bashrc
+
 if [ ! -d $HOME/.config/i3 ];then
     mkdir -p $HOME/.config/i3
 fi
-path="$HOME/dothub.d/data.d/Res"
-ln -sf $path/i3.res $HOME/.config/i3/config
-ln -sf $path/i3.status.toml $HOME/.config/i3/status.toml
-ln -sf $path/urxvt.res $HOME/.Xdefaults
-ln -sf $path/bash.res $HOME/.bashrc
+cat $path/i3.res > $HOME/.config/i3/config
+cat $path/i3.status.toml > $HOME/.config/i3/status.toml
+
+if [ ! -d $HOME/.config/sakura ];then
+    mkdir -p $HOME/.config/sakura
+fi
+cat $path/sakura.res > $HOME/.config/sakura/sakura.conf
