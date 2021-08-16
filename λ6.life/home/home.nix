785f5d25051554823c03c3d5b443cc6661962036
aylax <zhoubye@foxmail.com>
2021-08-16 13:17:39 +0800

    new :: nix os -> update configuration

--- λ6.life/home/home.nix
+++ λ6.life/home/home.nix
@@ -2,7 +2,7 @@
 let
   home-manager = builtins.fetchGit {
     url = "https://github.com/rycee/home-manager.git";
-    rev = "dd94a849df69fe62fe2cb23a74c2b9330f1189ed"; # the commit to fetch 
+    rev = "b39647e52ed3c0b989e9d5c965e598ae4c38d7ef"; # the commit to fetch 
     ref = "release-18.09"; # the branch to follow: release-xx.yy for stable nixos or master for nixos-unstable.
   };
 in
@@ -11,7 +11,7 @@ in
       (import "${home-manager}/nixos")
     ];
 
-    home-manager.users.my_username = {
+    home-manager.users.aylax = {
       /* Here goes your home-manager config, eg home.packages = [ pkgs.foo ]; */
     }
 }
---
+++ λ6.life/home/install.sh
@@ -0,0 +1,4 @@
+#!/user/bin sh
+mkdir -p $HOME/.config
+ln -s $(pwd) $HOME/.config
+nix-shell -p nixUnstable --command "nix build --experimental-features 'nix-command flakes' '.#$1'"
